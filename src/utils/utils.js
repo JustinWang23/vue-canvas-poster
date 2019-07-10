f4cf80fd92cf46418824286d15c19a59ac6aea79
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-10 07:49:23 +0000

    Deploy JustinWang23/Vue-Canvas-Poster to github.com/JustinWang23/Vue-Canvas-Poster.git:gh-pages

--- src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-// 重写alert框 去掉头部IP信息
-window.alert = function (name) {
-  var iframe = document.createElement('IFRAME')
-  iframe.style.display = 'none'
-  iframe.setAttribute('src', 'data:text/plain,')
-  document.documentElement.appendChild(iframe)
-  window.frames[0].window.alert(name)
-  iframe.parentNode.removeChild(iframe)
-}
